ecf928f6d1d7ed177b11912ad84c910d43920e23
Bella1703 <bella.vinogradova@gmail.com>
2020-12-29 09:00:37 +0300

    add first block

--- src/scripts/plugin.js
+++ src/scripts/plugin.js
@@ -1,1 +1,15 @@
-console.log("loading plugins");
+
+$('.first-block-main').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    prevArrow: ".first-block__arrow-left",
+    nextArrow: ".first-block__arrow-right",
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 7000,
+    draggable: true,
+    adaptiveHeight: true,
+});
+
